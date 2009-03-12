e6a022f812df0376a22feab8fd695b10cf5d8753
Harry Vangberg <harry@vangberg.name>
2009-03-12 14:52:25 +0100

    update

--- js/speecher.js
+++ js/speecher.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-  $(".slide").css("display", "none");
-  $(".step").parent().hide();
+  $(".slide").css("display", "none"); 
   $(".slide:first").css("display", "inherit");
+  $("#control a:first").addClass("active");
+  $(".step").parent().hide();
 
   $("body").click(function() {
     next_step_or_slide();
   })
+
   $("body").keydown(function(e) {
     switch (e.keyCode) {
       case 39:
@@ -14,6 +16,16 @@ $(document).ready(function() {
       case 37:
         previous_step_or_slide();
         break;
+    }
+  })
+
+  $("#control a").click(function(e) {
+    if ($(this).hasClass("active")) {
+      e.stop();
+    } else {
+      var next_id = $(this).html();
+      var next = $("#" + next_id);
+      transition(next);
     }
   })
 })
@@ -25,7 +37,7 @@ function next_step_or_slide() {
   if (next_step.length == 0) {
     var next_slide = current_slide.next(".slide:hidden:first");
     if (next_slide.length > 0) {
-      transition(current_slide, "left", next_slide, "right");
+      transition(next_slide);
     }
   } else {
     next_step.show();
@@ -40,15 +52,33 @@ function previous_step_or_slide() {
   if (visible_steps.length == 0) {
     var previous_slide = current_slide.prev(".slide");
     if (previous_slide.length > 0) {
-      transition(current_slide, "right", previous_slide, "left");
+      transition(previous_slide);
     }
   } else {
     current_step.hide();
   }
 }
 
-function transition(from, from_direction, to, to_direction) {
+function transition(to) {
+  from = $(".slide:visible");
+
+  if (from[0].id < to[0].id) {
+    // Forward
+    var from_direction = "left";
+    var to_direction = "right";
+  } else {
+    // Rewind
+    var from_direction = "right";
+    var to_direction = "left";
+  }
+
   from.hide("slide", { direction: from_direction }, 500);
   to.animate({border: "inherit"}, 500). // Stupid trick.
     show("slide", { direction: to_direction }, 500);
+  change_active_control(from, to);
+}
+
+function change_active_control(from, to) {
+  $("#control a[href=#" + from[0].id + "]").removeClass("active");
+  $("#control a[href=#" + to[0].id + "]").addClass("active");
 }
